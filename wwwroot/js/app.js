1bed88af8aac56addb8263068236ddf0dfb478bc
arunsinghdeveloper <arunsinghdeveloper@gmail.com>
2020-04-25 01:00:23 +0530

    Resolve routing issue if data is null

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,4 +1,18 @@
-function showMessage(Name, Email) {
+function saveMessage(Name, Email) {
     // alert("Record inserted successfully. Name is : " + Name + " and email id : " + Email);
-    document.getElementById("message").innerText = "Record inserted successfully. Name is : " + Name + " and email id : " + Email;
+    document.getElementById("messageBox").innerText = "Record inserted successfully. Name is : " + Name + " and email id : " + Email;
+}
+
+
+function SetFocusOnName(element) {
+
+    element.focus();
+     
+}
+
+function GetCity() {
+
+    var cities = ["Noida", "Delhi", "Gurugram", "Mumbai", "Lacknow"];
+
+    return cities;
 }
