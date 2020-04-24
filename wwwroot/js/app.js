004ce4aa50c57e35cf2afcf29e192dd295473962
arunsinghdeveloper <arunsinghdeveloper@gmail.com>
2020-04-24 23:32:12 +0530

    test

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,4 +1,4 @@
-
+function showMessage(Name, Email) {
     // alert("Record inserted successfully. Name is : " + Name + " and email id : " + Email);
     document.getElementById("message").innerText = "Record inserted successfully. Name is : " + Name + " and email id : " + Email;
 }
